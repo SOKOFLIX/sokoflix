f58dd7381f0fe0eb1137e2c93c70dbc1f627aece
Victor Soko <info@lgnd.co.za>
2026-03-24 02:19:34 +0200

    Silenced Vite chunk size warning

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    // This tells Vite not to warn us until the file hits a massive 1500kb!
+    chunkSizeWarningLimit: 1500, 
+  }
 })
